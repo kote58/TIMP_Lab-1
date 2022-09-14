82d36a2775f915e1a59f201fd0d26bbd4452cea6
kote58 <113284508+kote58@users.noreply.github.com>
2022-09-14 20:33:20 +0300

    Lab-1_2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <cctype>
-#include "modAlphaCipher.h"
+#include "BetaCipher.h"
 #include <locale>
 using namespace std;
-// проверка, чтобы строка состояла только из прописных букв
 bool isValid(const wstring& s)
 {
     for(auto c:s)
@@ -13,32 +12,28 @@ bool isValid(const wstring& s)
 }
 int main(int argc, char **argv)
 {
+    int key=3;
     locale loc("ru_RU.UTF-8");
     locale::global(loc);
-    wstring key;
+    wstring z;
     wstring text;
     unsigned op;
-    wcout<<L"Cipher ready. Input key: ";
-    wcin>>key;
-    if (!isValid(key)) {
-        wcerr<<L"key not valid\n";
-        return 1;
-    }
-    wcout<<L"Key loaded\n";
-    modAlphaCipher cipher(key);
+    wcout<<L"Cipher ready. Input please Password: ";
+    wcin>>z;
+    BetaCipher cipher(key);
     do {
-        wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        wcout<<L"Cipher ready. Input please operation (0-exit, 1-encrypt, 2-decrypt): ";
         wcin>>op;
         if (op > 2) {
             wcout<<L"Illegal operation\n";
         } else if (op >0) {
-            wcout<<L"Cipher ready. Input text: ";
+            wcout<<L"Cipher ready. Input please text: ";
             wcin>>text;
             if (isValid(text)) {
                 if (op==1) {
-                    wcout<<L"Encrypted text: "<<cipher.encrypt(text)<<endl;
+                    wcout<<L"Encrypted text: "<<cipher.CoderBetaCipher(cipher, text)<<endl;
                 } else {
-                    wcout<<L"Decrypted text: "<<cipher.decrypt(text)<<endl;
+                    wcout<<L"Decrypted text: "<<cipher.DecoderBetaCipher(cipher, text)<<endl;
                 }
             } else {
                 wcout<<L"Operation aborted: invalid text\n";
